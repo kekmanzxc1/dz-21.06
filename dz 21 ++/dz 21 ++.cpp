9c4219c82bd91097da211d5fc8c7e30982727a2b
kekmanzxc1 <stanislav61107@gmail.com>
2024-06-21 11:47:42 +0300

    Добавьте файлы проекта.

---
+++ dz 21 ++/dz 21 ++.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Drib {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Drib() : numerator(0), denominator(1) {}
+
+    void input() {
+        std::cout << "Enter the numerator:  ";
+        std::cin >> numerator;
+        std::cout << "Enter the denominator: ";
+        std::cin >> denominator;
+    }
+
+    void display() const {
+        std::cout << numerator << "/" << denominator << std::endl;
+    }
+};
+
+int main() {
+    Drib d;
+    std::cout << "Fraction:" << std::endl;
+    d.input();
+    std::cout << "Your fraction: ";
+    d.display();
+}
